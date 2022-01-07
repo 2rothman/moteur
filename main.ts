218a7981d7cdc1dccefee24d9553f42e3e4eb7f2
2rothman <92726613+2rothman@users.noreply.github.com>
2022-01-07 16:47:10 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,35 @@
 function Recul (vitesse: number) {
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    pins.digitalWritePin(DigitalPin.P2, 1)
-    pins.analogWritePin(AnalogPin.P12, vitesse)
+	
 }
-input.onButtonPressed(Button.A, function () {
-    if (vitesse < 1013) {
-        vitesse += 10
-    }
-    avance(vitesse)
-})
-input.onButtonPressed(Button.B, function () {
-    if (vitesse > 10) {
-        vitesse += -10
-        Recul(vitesse)
-    }
-})
 function avance (vitesse: number) {
-    pins.digitalWritePin(DigitalPin.P1, 1)
-    pins.digitalWritePin(DigitalPin.P2, 0)
-    pins.analogWritePin(AnalogPin.P12, vitesse)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.analogWritePin(AnalogPin.P0, vitesse)
 }
 let vitesse = 0
-vitesse = 0
+pins.analogWritePin(AnalogPin.P0, vitesse)
+pins.digitalWritePin(DigitalPin.P1, 0)
+pins.digitalWritePin(DigitalPin.P2, 1)
+basic.pause(200)
+while (vitesse < 500) {
+    vitesse += 10
+    pins.analogWritePin(AnalogPin.P0, vitesse)
+    basic.pause(200)
+}
+while (vitesse > 20) {
+    vitesse += -10
+    pins.analogWritePin(AnalogPin.P0, vitesse)
+    basic.pause(200)
+}
+pins.digitalWritePin(DigitalPin.P1, 1)
+pins.digitalWritePin(DigitalPin.P2, 0)
+while (vitesse < 500) {
+    vitesse += 10
+    pins.analogWritePin(AnalogPin.P0, vitesse)
+    basic.pause(200)
+}
+while (vitesse > 11) {
+    vitesse += -10
+    pins.analogWritePin(AnalogPin.P0, vitesse)
+    basic.pause(200)
+}
